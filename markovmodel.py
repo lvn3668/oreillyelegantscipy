375a5d9f132e9d9fbcd6d392e652f0fa4a66f571
lalitha.viswanathan79@gmail.com <lalitha.viswanathan79@gmail.com>
2021-09-22 12:10:41 -0400

    IMM Order 6

--- markovmodel.py
+++ markovmodel.py
@@ -3,6 +3,7 @@
 import string
 import numpy as np
 import toolz as tz
+from matplotlib import pyplot as plt, axis
 from toolz import curried as curry
 from glob import glob
 import itertools as itertoolz
@@ -87,6 +88,10 @@ def is_nucleotide(letter: chr, LDICTparam: dict):
 def increment_model(model: np.zeros, index: int):
     model[index] += 1
 
+#################################################################
+@tz.curry
+def calculateimmprob(model, index, markovmodel2, markovmodel3, markovmodel4, markovmodel5, markovmodel6):
+    model[index] +=markovmodel2+markovmodel3+markovmodel4+markovmodel5+markovmodel6
 
 #################################################################
 @tz.curry
@@ -101,8 +106,34 @@ def genome(file_pattern: str, LDICTparam: dict):
 
 
 #################################################################
+@tz.curry
+def immtestseq(seq: str, order: int, markovmodel2: dict, markovmodel3: dict, markovmodel4: dict, markovmodel5: dict, markovmodel6: dict):
+    model = np.zeros((4,4))
+    tz.last(tz.pipe(seq, curry.sliding_window(order),
+                    curry.map(markovmodel2.__getitem__(seq)),
+                    curry.map(markovmodel3.__getitem__(seq)),
+                    curry.map(markovmodel4.__getitem__(seq)),
+                    curry.map(markovmodel5.__getitem__(seq)),
+                    curry.map(markovmodel6.__getitem__(seq)),
+                    curry.map(calculateimmprob(model))))
+    np.seterr(invalid='ignore')
+    model /= np.sum(model, axis=1)[:, np.newaxis]
+#################################################################
 def read1k(f):
     return f.read(1024)
+#################################################################
+def plot_model(model, labels, figure=None):
+    fig = figure or plt.figure()
+    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    im = ax.imshow(model, cmap='magma')
+    axcolor = fig.add_axes([0.91, 0.1, 0.02, 0.8])
+    plt.colorbar(im, cax=axcolor)
+    for axis in [ax.xaxis, ax.yaxis]:
+        axis.set_ticks(range(4))
+        axis.set_ticks_position('none')
+        axis.set_ticklabels(labels)
+    plt.show()
+    return ax
 
 
 #################################################################
@@ -110,31 +141,35 @@ if __name__ == '__main__':
     drosophilamelanogaster: str = "C:\\Users\\visu4\\Documents\\wcgna\\data\\SRP140558\\dm6.fa"
     alphabetinfastafile: str = "atgc"
     lenalphabets = len(alphabetinfastafile)
-    LDICT, Nmerdicts = buildbasedictionaryandnmerdictionaries(alphabetinfastafile)
+    basedict, Nmerdicts = buildbasedictionaryandnmerdictionaries(alphabetinfastafile)
     print(Nmerdicts.keys())
     print(Nmerdicts[2])
 
     # I.e. pipe(data, f, g, h) is equivalent to h(g(f(data)))
-    markovmodel: dict = tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparam=LDICT), curry.take(10 ** 7),
+    markovmodel2: dict = tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparam=basedict), curry.take(10 ** 7),
                                 markov(order=2, PDICThash=Nmerdicts[2]))
-    print(markovmodel)
+    print(markovmodel2)
 
-    markovmodel: dict = tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparam=LDICT), curry.take(10 ** 7),
+
+    markovmodel3: dict = tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparam=basedict), curry.take(10 ** 7),
                                 markov(order=3, PDICThash=Nmerdicts[3]))
     print("Order 3")
-    print(markovmodel)
-
-    markovmodel: dict = tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparam=LDICT), curry.take(10 ** 7),
+    print(markovmodel3)
+    ax = plot_model(markovmodel3, labels='atgc')
+    markovmodel4: dict = tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparam=basedict), curry.take(10 ** 7),
                                 markov(order=4, PDICThash=Nmerdicts[4]))
     print("Order 4")
-    print(markovmodel)
+    print(markovmodel4)
 
-    markovmodel: dict = tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparam=LDICT), curry.take(10 ** 7),
+    markovmodel5: dict = tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparam=basedict), curry.take(10 ** 7),
                                 markov(order=5, PDICThash=Nmerdicts[5]))
     print("Order 5")
-    print(markovmodel)
+    print(markovmodel5)
 
-    markovmodel: dict = tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparan=LDICT), curry.take(10 ** 7),
+    markovmodel6: dict = tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparam=basedict), curry.take(10 ** 7),
                                 markov(order=6, PDICThash=Nmerdicts[6]))
     print("Order 6")
-    print(markovmodel)
+    print(markovmodel6)
+
+
+    #tz.pipe(drosophilamelanogaster.lower(), genome(LDICTparam=basedict), curry.take(10**7), immtestseq(6, markovmodel2, markovmodel3, markovmodel4, markovmodel5, markovmodel6))
